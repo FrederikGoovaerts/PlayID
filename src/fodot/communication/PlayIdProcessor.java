98cfe2bfe97989dabb90e38047d77c8a33fd68df
Thomas Winters <thomas.winters@student.kuleuven.be>
2015-01-23 15:28:08 +0100

    Improved the PlayID processor: You can give GDL file URI to main method.

--- src/fodot/communication/PlayIdProcessor.java
+++ src/fodot/communication/PlayIdProcessor.java
@@ -16,11 +16,52 @@ import fodot.exceptions.idp.IdpConnectionException;
 import fodot.exceptions.idp.IdpParseException;
 import fodot.exceptions.idp.NoValidModelsException;
 import fodot.exceptions.idp.UnsatisfiableIdpFileException;
+import fodot.exceptions.playid.PlayIdArgumentException;
 import fodot.gdl_parser.Parser;
 import fodot.objects.file.IFodotFile;
 import fodot.objects.structure.FodotStructure;
 
 public class PlayIdProcessor {
+	
+	private static final IActionOutputter DEFAULT_OUTPUTTER = new GdlActionPrinter();
+	private IActionOutputter outputter;
+	
+	/**********************************************
+	 *  Constructors
+	 ***********************************************/
+
+	public PlayIdProcessor(IActionOutputter outputter) {
+		super();
+		this.outputter = outputter;
+	}
+
+	public PlayIdProcessor() {
+		this(getDefaultOutputter());
+	}	
+
+	/**********************************************/
+
+	/**********************************************
+	 *  Outputter
+	 ***********************************************/
+
+	public IActionOutputter getOutputter() {
+		return outputter;
+	}
+
+	public void setOutputter(IActionOutputter outputter) {
+		this.outputter = outputter;
+	}	
+
+	public static IActionOutputter getDefaultOutputter() {
+		return DEFAULT_OUTPUTTER;
+	}
+
+	/**********************************************/
+
+	/**********************************************
+	 *  Process
+	 ***********************************************/
 	
 	public void process(File gdlFile)
 			throws IOException, IdpConnectionException,
@@ -43,8 +84,8 @@ public class PlayIdProcessor {
 		String idpResult = caller.callIDP(idpFile);
 
 		//Process results
-		IdpResultTransformer transformer = new IdpResultTransformer(parsedFodotFile, idpResult);
-		List<FodotStructure> models = transformer.getModels();
+		IdpResultTransformer resultTransformer = new IdpResultTransformer(parsedFodotFile, idpResult);
+		List<FodotStructure> models = resultTransformer.getModels();
 		
 		//Check if we found a model
 		if (models.size() == 0) {
@@ -56,13 +97,33 @@ public class PlayIdProcessor {
 		List<GdlAction> actions = answerer.generateActionSequence();
 		
 		//Output it
-		IActionOutputter outputter = new GdlActionPrinter(actions);
-		outputter.output();
+		outputter.output(actions);
 	}
+
+	/**********************************************/
+
+	/**********************************************
+	 *  Main method
+	 ***********************************************/
 	
 	public static void main(String[] args) throws IOException {
-		new PlayIdProcessor().process( new File("resources/games/maze.kif"));
+		if (args.length <= 0) {
+			throw new PlayIdArgumentException("Please give the uri of a valid GDL file to the PlayID processor.");
+		}
+		if (!args[0].contains(".kif")) {
+			throw new PlayIdArgumentException("The given uri must lead to a .kif file.");
+		}
+		File gdlFile = new File(args[0]);
+		if (!gdlFile.exists()) {
+			throw new PlayIdArgumentException("The given uri must lead to a existing file.");
+		}
+		
+		PlayIdProcessor processor = new PlayIdProcessor();
+		processor.process(gdlFile);
 	}
+	
+
+	/**********************************************/
 	
 	
 }
