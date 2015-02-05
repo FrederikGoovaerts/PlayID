c6e2dc7b5ff01bd2a5218d25ff1dd7a918d6ac13
Thomas Winters <thomas.winters@hotmail.com>
2015-02-05 16:57:27 +0100

    Added FodotElementCollection as superclass for the container and list with similar names.

--- src/fodot/objects/general/FodotElementContainer.java
+++ src/fodot/objects/general/FodotElementContainer.java
@@ -4,104 +4,26 @@ import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import fodot.exceptions.fodot.FodotException;
-
-public abstract class FodotElementContainer<E extends IFodotElement> extends FodotElement implements IFodotElement {
-
-	private Set<E> elements;
+public abstract class FodotElementContainer<E extends IFodotElement> extends FodotElementCollection<E> implements IFodotElement {
 
 	public FodotElementContainer(Collection<? extends E> elements) {
-		super();
-		setElements(elements);
+		super(elements, -1, -1);
 	}
 
 	/**********************************************
 	 *  Elements methods
 	 ***********************************************/
-	protected void setElements(Collection<? extends E> argElements) {
-		if (this.elements != null && this.elements.equals(argElements)){
-			return;
-		}
-		this.elements = new LinkedHashSet<E>();
-		addAllElements(argElements);
+
+	@Override
+	public Set<E> getElements() {
+		return new LinkedHashSet<E>(super.getElements());
 	}
 
-	public Set<E> getElements() {
+	@Override
+	protected Set<E> wrapInDefaultCollection(Collection<? extends E> elements) {
 		return new LinkedHashSet<E>(elements);
 	}
-
-	@Override
-	public Collection<? extends IFodotElement> getDirectFodotElements() {
-		return getElements();
-	}
-
-	public void addElement(E argElement) {
-		if (!isValidElement(argElement)) {
-			throw new FodotException(argElement + " is not a valid element for " + this);
-		
-		}
-		this.elements.add(argElement);
-	}
-
-	public abstract boolean isValidElement(E argElement);
 	
-	public void addAllElements(Collection<? extends E> argElements) {
-		if (argElements != null) {
-			for (E el : argElements) {
-				addElement(el);
-			}
-		}
-	}
-
-	public boolean containsElement(E element) {
-		return this.elements.contains(element);
-	}
-
-	public boolean containsAllElements(Collection<? extends E> argElements) {
-		return this.elements.containsAll(argElements);
-	}
-	
-	public boolean hasElements() {
-		return !elements.isEmpty();
-	}
-
-	public void removeElement(E argElement) {
-		this.elements.remove(argElement);
-	}
-
-	public int getAmountOfElements() {
-		return this.elements.size();
-	}
-	
-	/**********************************************/
-
-
-	//Hashcode&Equals
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((elements == null) ? 0 : elements.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		FodotElementContainer<?> other = (FodotElementContainer<?>) obj;
-		if (elements == null) {
-			if (other.elements != null)
-				return false;
-		} else if (!elements.equals(other.elements))
-			return false;
-		return true;
-	}
-	
+	/**********************************************/	
 
 }
--- src/fodot/objects/general/FodotElementList.java
+++ src/fodot/objects/general/FodotElementList.java
@@ -4,120 +4,42 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import fodot.exceptions.fodot.FodotException;
+public abstract class FodotElementList<E extends IFodotElement> extends FodotElementCollection<E> implements IFodotElement {
+	
+	public FodotElementList(Collection<? extends E> elements, int minSize, int maxSize) {
+		super(elements, minSize, maxSize);
+	}
 
-public abstract class FodotElementList<E extends IFodotElement> extends FodotElement implements IFodotElement {
-	private List<E> elements;
+	public FodotElementList(Collection<? extends E> elements, int maxSize) {
+		this(elements, -1, maxSize);
+	}
 	
 	public FodotElementList(Collection<? extends E> elements) {
-		super();
-		setElements(elements);
+		this(elements, -1, -1);
 	}
 
 	/**********************************************
 	 *  Elements methods
 	 ***********************************************/
-	private void setElements(Collection<? extends E> argElements) {
-		if (this.elements != null && this.elements.equals(argElements)){
-			return;
-		}
-		this.elements = new ArrayList<E>();
-		addAllElements(argElements);
+
+	@Override
+	public List<E> getElements() {
+		return new ArrayList<E>(super.getElements());
+	}
+	
+	public E getElement(int index) {
+		return getElements().get(index);
+	}
+	
+	public void setElement(int index, E element) {
+		( (List<E>) (super.getElements())).set(index, element);
 	}
 
-	public List<E> getElements() {
+	@Override
+	protected List<E> wrapInDefaultCollection(Collection<? extends E> elements) {
 		return new ArrayList<E>(elements);
 	}
 	
-	@Override
-	public Collection<? extends IFodotElement> getDirectFodotElements() {
-		return getElements();
-	}
-
-
-	public E getElement(int index) {
-		return elements.get(index);
-	}
-	
-	public void addElement(E argElement) {
-		if (!isValidElement(argElement)) {
-			throw new FodotException(argElement + " is not a valid argument for " + this);
-		
-		}
-		this.elements.add(argElement);
-	}
-
-	public abstract boolean isValidElement(E argElement);
-	
-	public void addAllElements(Collection<? extends E> argElements) {
-		if (argElements != null) {
-			for (E el : argElements) {
-				addElement(el);
-			}
-		}
-	}
-
-	public boolean containsElement(E element) {
-		return this.elements.contains(element);
-	}
-
-	public boolean hasElements() {
-		return !elements.isEmpty();
-	}
-
-	public void removeElement(E argElement) {
-		this.elements.remove(argElement);
-	}
-
-	public int getAmountOfElements() {
-		return this.elements.size();
-	}
-	
-	public List<? extends IFodotElement> getElementsOfClass(Class<? extends IFodotElement> clazz) {
-		if (clazz == null) {
-			return new ArrayList<E>();
-		}
-		List<E> result = new ArrayList<E>();
-		for (E el : elements) {
-			if (clazz.isInstance(el)) {
-				result.add(el);
-			}
-		}
-		return result;
-	}
-	/**********************************************/
-
-	
-	/**********************************************
-	 *  Obligatory methods
-	 ***********************************************/
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((elements == null) ? 0 : elements.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		FodotElementList<?> other = (FodotElementList<?>) obj;
-		if (elements == null) {
-			if (other.elements != null)
-				return false;
-		} else if (!elements.equals(other.elements))
-			return false;
-		return true;
-	}
-
 	/**********************************************/
 
 
