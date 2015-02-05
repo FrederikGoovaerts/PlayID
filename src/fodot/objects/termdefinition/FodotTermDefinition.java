89feff3e4a0b296305b3883379669472a2fff3a0
Thomas Winters <thomas.winters@hotmail.com>
2015-02-05 17:30:06 +0100

    Added FodotTermDefinition

---
+++ src/fodot/objects/termdefinition/FodotTermDefinition.java
@@ -0,0 +1,31 @@
+package fodot.objects.termdefinition;
+
+import java.util.Arrays;
+
+import fodot.objects.file.FodotFileElement;
+import fodot.objects.file.IFodotFileElement;
+import fodot.objects.theory.elements.terms.IFodotTerm;
+import fodot.objects.vocabulary.FodotVocabulary;
+
+public class FodotTermDefinition extends FodotFileElement<IFodotTerm> implements IFodotFileElement {
+	
+	public FodotTermDefinition(String name, IFodotTerm term, FodotVocabulary voc) {
+		super(name, Arrays.asList(term), voc);
+	}
+
+	@Override
+	public String getFileElementName() {
+		return "term";
+	}
+
+	@Override
+	public String getDefaultName() {
+		return "t";
+	}
+
+	@Override
+	public boolean isValidElement(IFodotTerm argElement) {
+		return getSize() <= 0;
+	}
+
+}
