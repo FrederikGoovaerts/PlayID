3343dfc9e285eda92b74f99206c2b19a3ce803e4
Thomas Winters <thomas.winters@student.kuleuven.be>
2015-02-11 01:11:44 +0100

    Added singleplayer typeidentification test.

---
+++ src/fodot/tests/type_identifier/SinglePlayerTypeIdentification.java
@@ -0,0 +1,24 @@
+package fodot.tests.type_identifier;
+
+import java.io.File;
+
+import fodot.gdl_parser.GdlParser;
+import fodot.gdl_parser.GdlVocabulary;
+import fodot.gdl_parser.firstphase.GdlTypeIdentifier;
+import fodot.gdl_parser.visitor.GdlInspector;
+import fodot.tests.transformation.SingleplayerTransformationTest;
+
+public class SinglePlayerTypeIdentification extends
+SingleplayerTransformationTest {
+
+	@Override
+	protected void testFor(String gameName) {
+		File toParse = toFile(gameName);
+		GdlParser parser = new GdlParser(toParse);
+		
+		GdlTypeIdentifier identifier = new GdlTypeIdentifier();
+		GdlInspector.inspect(parser.getGame(), identifier.createTransformer());
+		GdlVocabulary gdlVocabulary = identifier.generateTranslationData();
+	}
+
+}
