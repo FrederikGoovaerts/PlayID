1661026b5328ec80bf9bb6767675159d26387853
Thomas Winters <thomas.winters@student.kuleuven.be>
2015-02-11 17:36:31 +0100

    Made FO(.) objects sort their direct elements in alphanumeric order, which adds more readability when debugging.

--- src/fodot/objects/general/sorting/FodotElementComparators.java
+++ src/fodot/objects/general/sorting/FodotElementComparators.java
@@ -8,31 +8,34 @@ import fodot.objects.theory.elements.terms.FodotConstant;
 import fodot.objects.theory.elements.terms.FodotVariable;
 
 public class FodotElementComparators {
+	
+	private static final Comparator<String> alphanumComp = new AlphanumComparator();
+	
 	public static final Comparator<FodotVariable> VARIABLE_NAME_COMPARATOR = new Comparator<FodotVariable>() {
 		@Override
 		public int compare(FodotVariable o1, FodotVariable o2) {
-			return o1.getName().compareTo(o2.getName());
+			return alphanumComp.compare(o1.getName(), o2.getName());
 		}
 	};
 	
 	public static final Comparator<FodotConstant> CONSTANT_VALUE_COMPARATOR = new Comparator<FodotConstant>() {
 		@Override
 		public int compare(FodotConstant o1, FodotConstant o2) {
-			return o1.getValue().compareTo(o2.getValue());
+			return alphanumComp.compare(o1.getValue(), o2.getValue());
 		}
 	};
 	
 	public static final Comparator<IFodotElement> ELEMENT_TOCODE_COMPARATOR = new Comparator<IFodotElement>() {
 		@Override
 		public int compare(IFodotElement o1, IFodotElement o2) {
-			return o1.toCode().trim().compareTo(o2.toCode().trim());
+			return alphanumComp.compare(o1.toCode().trim(), o2.toCode().trim());
 		}
 	};
 	
 	public static final Comparator<IFodotEnumerationElement> ENUMERATION_ELEMENT_COMPARATOR = new Comparator<IFodotEnumerationElement>() {
 		@Override
 		public int compare(IFodotEnumerationElement o1, IFodotEnumerationElement o2) {
-			return o1.getValue().trim().compareTo(o2.getValue().trim());
+			return alphanumComp.compare( o1.getValue().trim(), o2.getValue().trim());
 		}
 	};
 	
