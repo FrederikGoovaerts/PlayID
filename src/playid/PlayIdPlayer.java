f9565166be2b29fa112b156f9ff43bcdd1503432
Thomas Winters <thomas.winters@student.kuleuven.be>
2015-03-31 17:03:09 +0200

    Implemented PlayIdPlayer

--- src/playid/PlayIdPlayer.java
+++ src/playid/PlayIdPlayer.java
@@ -1,60 +1,69 @@
 package playid;
 
+import java.io.IOException;
+
 import org.ggp.base.player.gamer.exception.GamePreviewException;
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
+import org.ggp.base.util.match.Match;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+import playid.domain.PlayIdProcessor;
+import playid.domain.gdl_transformers.movesequence.MoveSequence;
 
 public class PlayIdPlayer extends StateMachineGamer {
 
+	private PlayIdProcessor processor;
+	
 	@Override
 	public StateMachine getInitialStateMachine() {
-		// TODO Auto-generated method stub
-		return null;
+		 return new ProverStateMachine();
 	}
 
 	@Override
 	public void stateMachineAbort() {
-		// TODO Auto-generated method stub
-
+		//EMPTY
 	}
 
 	@Override
 	public void stateMachineMetaGame(long arg0)
 			throws TransitionDefinitionException, MoveDefinitionException,
 			GoalDefinitionException {
-		// TODO Auto-generated method stub
-
+		Match match = getMatch();
+		processor = new PlayIdProcessor(match.getGame(), getRole());
 	}
 
 	@Override
 	public Move stateMachineSelectMove(long arg0)
 			throws TransitionDefinitionException, MoveDefinitionException,
 			GoalDefinitionException {
-		// TODO Auto-generated method stub
-		return null;
+		MoveSequence moveSequence;
+		try {
+			moveSequence = processor.calculateNextMove(MoveSequence.MoveSequenceBuilder.fromTermListList(getMatch().getMoveHistory()));
+		} catch (IOException e) {
+			throw new RuntimeException("PLAYID HAD AN EXCEPTION!", e);
+		}
+		return moveSequence.getMove(moveSequence.getAmountOfMoves()-1, getRoleName());
 	}
 
 	@Override
 	public void stateMachineStop() {
-		// TODO Auto-generated method stub
-
+		//EMPTY
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "PlayId v0.1";
 	}
 
 	@Override
 	public void preview(Game arg0, long arg1) throws GamePreviewException {
-		// TODO Auto-generated method stub
-
+		//EMPTY
 	}
 
 }
