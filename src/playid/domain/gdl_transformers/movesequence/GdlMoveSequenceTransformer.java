4076e67d22df14234db64253c76d1de8ad16c705
Thomas Winters <thomas.winters@student.kuleuven.be>
2015-03-31 16:35:54 +0200

    Implemented GdlMoveSequenceTransformer.

--- src/playid/domain/gdl_transformers/movesequence/GdlMoveSequenceTransformer.java
+++ src/playid/domain/gdl_transformers/movesequence/GdlMoveSequenceTransformer.java
@@ -1,6 +1,7 @@
 package playid.domain.gdl_transformers.movesequence;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import org.ggp.base.util.statemachine.Move;
 
 import playid.domain.fodot.FodotElementBuilder;
 import playid.domain.fodot.structure.elements.predicateenum.FodotPredicateEnumeration;
+import playid.domain.fodot.structure.elements.predicateenum.elements.FodotPredicateEnumerationElement;
 import playid.domain.fodot.structure.elements.predicateenum.elements.IFodotPredicateEnumerationElement;
 import playid.domain.fodot.structure.elements.typeenum.elements.IFodotTypeEnumerationElement;
 import playid.domain.gdl_transformers.second_phase.GdlFodotSentenceTransformer;
@@ -26,23 +28,40 @@ public class GdlMoveSequenceTransformer {
 			MoveSequence moves) {
 
 		List<IFodotPredicateEnumerationElement> fodotMoves = new ArrayList<>();
-
 		GdlFodotSentenceTransformer actionTranslator = new GdlFodotSentenceTransformer(
 				transformer);
 
 		for (int i = 0; i < moves.getAmountOfMoves(); i++) {
+			
+			//Save the current time in a variable
 			IFodotTypeEnumerationElement time = FodotElementBuilder
 					.createInteger(i).toEnumerationElement();
 			Collection<Move> movesAtMoment = moves.getMoves(i);
+			
+			
 			for (Move move : movesAtMoment) {
+				
+				//Extract GDL contents: player&action
 				GdlTerm doPred = move.getContents();
-				GdlConstant player = (GdlConstant) doPred.toSentence().get(0);
-				GdlTerm action = doPred.toSentence().get(1);
+				GdlConstant gdlPlayer = (GdlConstant) doPred.toSentence()
+						.get(0);
+				GdlTerm gdlAction = doPred.toSentence().get(1);
 
+				//Translate gdl player&action to fodot player&action
 				IFodotTypeEnumerationElement fodotPlayer = actionTranslator
-						.generateConstant(player, transformer.getPlayerType()).toEnumerationElement();
-				IFodotTypeEnumerationElement fodotAction =
-						actionTranslator.generateTerm(action, transformer.getActionType()).toEnumerationElement();
+						.generateConstant(gdlPlayer,
+								transformer.getPlayerType())
+						.toEnumerationElement();
+				IFodotTypeEnumerationElement fodotAction = actionTranslator
+						.generateTerm(gdlAction, transformer.getActionType())
+						.toEnumerationElement();
+
+				List<IFodotTypeEnumerationElement> predicateElements = Arrays
+						.asList(time, fodotPlayer, fodotAction);
+
+				// Add to moves
+				fodotMoves.add(new FodotPredicateEnumerationElement(transformer
+						.getDoPredicateDeclaration(), predicateElements));
 			}
 		}
 
