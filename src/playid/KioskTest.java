8817789178383b99784103b1a5cd40e816cdb686
Thomas Winters <thomas.winters@student.kuleuven.be>
2015-04-09 23:27:41 +0200

    Added class that can experiment with GGP servers & players.

--- src/playid/KioskTest.java
+++
@@ -1,9 +0,0 @@
-package playid;
-
-import org.ggp.base.apps.kiosk.Kiosk;
-
-public class KioskTest {
-	public static void main(String[] args) {
-		Kiosk.main(args);
-	}
-}
---
+++ src/playid/MatchTester.java
@@ -0,0 +1,13 @@
+package playid;
+
+import java.io.IOException;
+
+import org.ggp.base.apps.player.Player;
+import org.ggp.base.apps.server.Server;
+
+public class MatchTester {
+	public static void main(String[] args) throws IOException {
+		Server.main(new String[0]);
+		Player.main(new String[0]);
+	}
+}
